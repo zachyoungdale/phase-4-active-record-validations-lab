9aadfa135fcdf0f9c36222e429367fb7ca19ac1b
Zachary Youngdale <zyoungdale@gmail.com>
2022-11-01 17:02:18 -0400

    commit tree 7e36fd5f8be72dbb7b3cc35c4c5c3ced7122bb29

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,31 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: ['Fiction', 'Non-Fiction']}
+    validate :clickbait?
+
+    
+
+
+    def clickbait?
+        # clickbait_patterns = ["Won't Believe", "Secret", "Top [number]", "Guess"]
+        # if self.title == nil
+        #     return
+        # end
+        # if clickbait_patterns.none? {|pattern| title.include?(pattern)}
+        #     errors.add(:title, "title is not clickbait")
+        # end
+        if !title
+            return
+        end
+       unless self.title.include?("Won't Believe" || "Secret" || "Top [number]" || "Guess")
+        errors.add(:title, "make it juicier")
+       end
+    end
+
+
+
+
+
 end
